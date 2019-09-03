2ae33745f51b2ce7404261b5c6515f397586eeb4
林强 <linqiang@eastcom.com>
2019-09-03 17:05:18 +0800

    2

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,43 +1,62 @@
 <template>
-<vue-fab 
-  :mainBtnColor="mainBtnColor" 
-  :zIndex="zIndex" 
-  :icon="icon" 
-  :size="size" 
-  :activeIcon="activeIcon">
-  <fab-item 
-    @clickItem="clickItem"
-    :idx="fabItem.idx" 
-    :title="fabItem.title" 
-    :icon="fabItem.icon" 
-    :color="fabItem.color" 
-    :titleColor="fabItem.titleColor" 
-    :titleBgColor="fabItem.titleBgColor" />
+  <div class="main">
+
+      <i class="icons iconfont material-icons vue-fab-iconfont-icons icon-add" ></i>
+    <vue-fab icon="icon-add" 
+  fabItemAnimate="default" 
+  mainBtnColor="#16C2C2">
+  <fab-item @clickItem="clickBABA" icon="icon-add" :idx="0" title="iconfont.cn">
+  </fab-item>
+  <fab-item @clickItem="clickBABA" icon="icon-add" :idx="1" title="交换">
+  </fab-item>
+  <fab-item @clickItem="clickBABA" icon="icon-add" :idx="2" title="聚焦">
+  </fab-item>
 </vue-fab>
+  </div>
 </template>
+
 <script>
-export default {
-  data () {
-    return {
-      icon: 'icon-Ruler',
-      activeIcon: 'icon-Ruler',
-      size: 'normal',
-      mainBtnColor: '#E64C3B',
-      zIndex: 5,
-      fabItem: {
-        idx: 1,
-        title: '这是一个演示的标题',
-        icon: 'icon-Ruler',
-        color: '#C7D23B',
-        titleColor: '#666',
-        titleBgColor: '#FFF'
+  export default {
+    data () {
+      return {
+      }},
+    methods: {
+      clickItem: function (params) {
+        window.alert('idx: ' + params.idx)
+      },
+      clickBABA: function () {
+        window.open('https://www.iconfont.cn/')
+      },
+      clickMainBtn: function () {
+        window.alert('主菜单为空时点击主Fab会触发clickMainBtn事件')
+      },
+      hidenFab: function () {
+        this.$refs.vuefab.onOffFab(false)
+      },
+      copyCode: function (code) {
+        this.$refs.textarea.value = code
+        this.$refs.textarea.select()
+        document.execCommand('Copy')
+        this.$refs.textarea.value = ''
+        window.alert('复制成功')
+      },
+      showCode: function (item) {
+        if (item.isShow) {
+          item.isShow = false
+          return
+        }
+        this.democode.forEach((item) => {
+          item.isShow = false
+        })
+        item.isShow = !item.isShow
       }
-    }
-  },
-  methods: {
-    clickItem (item) {
-      window.alert(item.idx)
+    },
+    created () {
+
     }
   }
-}
 </script>
+
+<style scoped>
+
+</style>
