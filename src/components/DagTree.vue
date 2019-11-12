83568facc5a47b4d188b0ff75a5883c14a3e8785
林强-f <linqiang-f@rt.com>
2019-11-12 11:15:24 +0800

    树形

--- src/components/DagTree.vue
+++ src/components/DagTree.vue
@@ -16,9 +16,12 @@ import { Tree  } from 'iview';
                         children: [],
                         checked:true,
                         id:0,
+                      
                     }
                      
-                ]
+                ],
+                uncheckedEnitity:[],
+                uncheckedtype:[],
             }
         },
         mounted(){
@@ -32,7 +35,7 @@ import { Tree  } from 'iview';
                  console.info(item);
                  let type=item.type;
                  let entity=item.entityid;
-                  Vue.axios.get('/Handlers/MVCEasy.ashx', {
+                  Vue.axios.get('/app/data/json/tree.json', { //'/app/data/json/tree.json','/Handlers/MVCEasy.ashx'
                             params: {
                                 ctrl:'DialPadDao',
                                 action: "GetTreeChildrenNode",
@@ -41,7 +44,6 @@ import { Tree  } from 'iview';
                                 gettime:new Date().getTime()  
                             }
                           }).then((res) => {
-                                debugger;  
                               if (res.data!=''){  
                                                         
                               callback(res.data);
@@ -75,32 +77,88 @@ import { Tree  } from 'iview';
                 }
             },
             checkchange(array,item){
-                 for (let i = array.length-1; i >=0; i--){
-                    if(!array[i].children) continue;
-                   for (let n = 0; n <array[i].children.length; n++){
-                       let index =this.checkparent(array,array[i].children[n]); //检查父节点是否已经选中，如果已经包含则该节点排除
-                       array.splice(index,1); 
-                   }       
-                 }
-           
-                  let zhishuString="";
-                  let entityString="";
-                  let typeString="";
-                  for (let i = array.length-1; i >=0; i--){
-                      switch (array[i].type) {
-                          case "zhishuuser":
-                             zhishuString+=array[i].id+',';
+                console.info(array);
+                console.info(item);
+                 if (item.checked==true){  //选中
+                    switch (item.type){
+                           case "zhishuuser":
+                              this.uncheckedEnitity.forEach(function(thisitem, index, arr) {
+                                    if(thisitem=item.id) {
+                                        arr.splice(index, 1);
+                                    }
+                                });
                               break;
                           case "usertype":
-                              typeString+=array[i].entityid+":"+array[i].id+';';
+                               this.uncheckedtype.forEach(function(thisitem, index, arr) {
+                                    if(thisitem==item.entityid+":"+item.id) {
+                                        arr.splice(index, 1);
+                                    }
+                                });
+                          // this.uncheckedtype.replace(item.entityid+":"+item.id+";","")
                               break;  
                           default:
-                              entityString+=array[i].id+',';
+                           this.uncheckedEnitity=this.uncheckedEnitity.filter(function(thisitem){
+                               return item.child.indexOf(thisitem)<0;
+                           })
                               break;
-                      }
-                 }
-                 let returnString =zhishuString+"/"+entityString+"/"+typeString;
-                 checkedEntity = returnString;//全局赋值单位选择，赋值原有全局属性值
+                    }
+                    
+                 } 
+                 else   //去选中
+                 {
+                     switch (item.type){
+                           case "zhishuuser":
+                             this.uncheckedEnitity.push(parseInt(item.id));
+                              break;
+                          case "usertype":
+                               this.uncheckedtype.push(item.entityid+":"+item.id);                       
+                              break;  
+                          default:
+                              this.uncheckedEnitity=this.uncheckedEnitity.concat(item.child);
+                            break;
+                    }
+
+                 }  
+
+                console.info(this.uncheckedEnitity);
+                console.info(this.uncheckedtype);
+                let returnval='/';
+                this.uncheckedEnitity.forEach(function(thisitem) {
+                           returnval+=thisitem+',';   
+                     });
+                returnval+='/';
+                this.uncheckedtype.forEach(function(thisitem) {
+                           returnval+=thisitem+';';   
+                     });
+                uncheckedEnitity=returnval;
+
+                //以下是原有发送选中的方法
+                //  for (let i = array.length-1; i >=0; i--){
+                //     if(!array[i].children) continue;
+                //    for (let n = 0; n <array[i].children.length; n++){
+                //        let index =this.checkparent(array,array[i].children[n]); //检查父节点是否已经选中，如果已经包含则该节点排除
+                //        array.splice(index,1); 
+                //    }       
+                //  }
+           
+                //   let zhishuString="";
+                //   let entityString="";
+                //   let typeString="";
+                //   for (let i = array.length-1; i >=0; i--){
+                //       switch (array[i].type) {
+                //           case "zhishuuser":
+                //              zhishuString+=array[i].id+',';
+                //               break;
+                //           case "usertype":
+                //               typeString+=array[i].entityid+":"+array[i].id+';';
+                //               break;  
+                //           default:
+                //               entityString+=array[i].id+',';
+                //               break;
+                //       }
+                //  }
+                //  let returnString =zhishuString+"/"+entityString+"/"+typeString;
+                 //checkedEntity = returnString;//全局赋值单位选择，赋值原有全局属性值
                  ReLoadUser();//刷新地图人员，调用原有方法
             },
            checkparent(array,item){
@@ -112,7 +170,7 @@ import { Tree  } from 'iview';
            },
            initEntity(){
                let _this=this;
-               Vue.axios.get('/Handlers/MVCEasy.ashx', {
+               Vue.axios.get('/app/data/json/tree.json', { //'/app/data/json/tree.json','/Handlers/MVCEasy.ashx'
                             params: {
                                 ctrl:'DialPadDao',
                                 action: "GetTreeChildrenNode",
@@ -120,8 +178,7 @@ import { Tree  } from 'iview';
                                 type:"entity"  
                             }
                           }).then((res) => {
-                              if (res.data!=''){  
-                                  debugger;                          
+                              if (res.data!=''){                         
                               _this.data3=res.data;
                               }
                           }).catch((err) => {
